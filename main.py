a099e74fbc85651c9c01eb6fbaafffb2d64ba6e4
Simon Waldherr <git@simon.waldherr.eu>
2024-07-18 19:25:22 +0200

    fix simon game small improvements

--- main.py
+++ main.py
@@ -186,6 +186,8 @@ def get_joystick_direction():
             return JOYSTICK_UP
     return None
 
+rtc = machine.RTC()
+
 # Game Selector
 def game_selector():
     display.start()
@@ -214,74 +216,133 @@ def game_selector():
 # Simon Game
 def simon_game():
     def draw_quad_screen():
-        rect(0, 0, WIDTH // 2 - 1, HEIGHT // 2 - 1, *inactive_colors[0])
-        rect(WIDTH // 2, 0, WIDTH - 1, HEIGHT // 2 - 1, *inactive_colors[1])
-        rect(0, HEIGHT // 2, WIDTH // 2 - 1, HEIGHT - 1, *inactive_colors[2])
-        rect(WIDTH // 2, HEIGHT // 2, WIDTH - 1, HEIGHT - 1, *inactive_colors[3])
-
+        rect(0, 0, WIDTH // 2 - 1, (HEIGHT-6) // 2 - 1, *inactive_colors[0])
+        rect(WIDTH // 2, 0, WIDTH - 1, (HEIGHT-6) // 2 - 1, *inactive_colors[1])
+        rect(0, (HEIGHT-6) // 2, WIDTH // 2 - 1, (HEIGHT-6) - 1, *inactive_colors[2])
+        rect(WIDTH // 2, (HEIGHT-6) // 2, WIDTH - 1, (HEIGHT-6) - 1, *inactive_colors[3])
+        
     def flash_color(index, duration=0.5):
         x, y = index % 2, index // 2
-        rect(x * WIDTH // 2, y * HEIGHT // 2, (x + 1) * WIDTH // 2 - 1, (y + 1) * HEIGHT // 2 - 1, *colors[index])
+        rect(x * WIDTH // 2, y * (HEIGHT-6) // 2, (x + 1) * WIDTH // 2 - 1, (y + 1) * (HEIGHT-6) // 2 - 1, *colors[index])
         time.sleep(duration)
-        rect(x * WIDTH // 2, y * HEIGHT // 2, (x + 1) * WIDTH // 2 - 1, (y + 1) * HEIGHT // 2 - 1, *inactive_colors[index])
-
+        rect(x * WIDTH // 2, y * (HEIGHT-6) // 2, (x + 1) * WIDTH // 2 - 1, (y + 1) * (HEIGHT-6) // 2 - 1, *inactive_colors[index])
+        
     def play_sequence():
         for color in simon_sequence:
             flash_color(color)
             time.sleep(0.5)
-
+            
+    def get_joystick_direction():
+        read0 = adc0.read_u16()
+        read1 = adc1.read_u16()
+        read2 = adc2.read_u16()
+        
+        valueX = read0 - 32768  # Adjusted to be centered at 0
+        valueY = read1 - 32768  # Adjusted to be centered at 0
+        
+        if abs(valueX) > abs(valueY):
+            if valueX > 10000:
+                return 'RIGHT'
+            elif valueX < -10000:
+                return 'LEFT'
+        else:
+            if valueY > 10000:
+                return 'DOWN'
+            elif valueY < -10000:
+                return 'UP'
+            
+        return None
+    
+    def get_user_input():
+        while True:
+            joystick_dir = get_joystick_direction()
+            if joystick_dir:
+                return joystick_dir
+            time.sleep(0.1)
+            
     def translate_joystick_to_color(joystick_dir):
-        if joystick_dir == JOYSTICK_UP:
+        if joystick_dir == 'UP':
             return 0
-        elif joystick_dir == JOYSTICK_RIGHT:
+        elif joystick_dir == 'RIGHT':
             return 1
-        elif joystick_dir == JOYSTICK_LEFT:
+        elif joystick_dir == 'LEFT':
             return 2
-        elif joystick_dir == JOYSTICK_DOWN:
+        elif joystick_dir == 'DOWN':
             return 3
         return None
-
+    
     def check_user_sequence():
         for i in range(len(user_sequence)):
             if user_sequence[i] != simon_sequence[i]:
                 return False
         return True
-
+    
     def start_game():
         global simon_sequence, user_sequence
         simon_sequence = []
         user_sequence = []
         draw_quad_screen()
-
+        
+    def joystick_test():
+        start_time = time.time()
+        while time.time() - start_time < 10:
+            joystick_dir = get_joystick_direction()
+            if joystick_dir:
+                print(f"Joystick moved: {joystick_dir}")
+                color_index = translate_joystick_to_color(joystick_dir)
+                if color_index is not None:
+                    flash_color(color_index, 0.5)
+            time.sleep(0.1)
+        print("Joystick test completed")
+        
+    def display_score_and_time(score):
+        global text
+        year, month, day, wd, hour, minute, second, _ = rtc.datetime()
+        time_str = "{:02}:{:02}".format(hour, minute)
+        score_str = str(score)
+        time_x = WIDTH - (len(time_str) * 6)
+        time_y = HEIGHT - 6
+        score_x = 1
+        score_y = HEIGHT - 6
+        if text != score_str + " " + time_str:
+            text = score_str + " " + time_str
+            rect(score_x, score_y, WIDTH, score_y + 5, 0, 0, 0)
+        draw_text_small(score_x, score_y, score_str, 255, 255, 255)
+        draw_text_small(time_x, time_y, time_str, 255, 255, 255)
+        
     def main_simon_game_loop():
         global simon_sequence, user_sequence
-
+        
         start_game()
+        #joystick_test()
         while True:
             simon_sequence.append(random.randint(0, 3))
+            display_score_and_time(len(simon_sequence) - 1)
             play_sequence()
             user_sequence = []
-
+            
             for _ in range(len(simon_sequence)):
-                joystick_dir = get_joystick_direction()
+                joystick_dir = get_user_input()
                 selected_color = translate_joystick_to_color(joystick_dir)
                 if selected_color is not None:
                     flash_color(selected_color, 0.2)
                     user_sequence.append(selected_color)
+                    if not check_user_sequence():
+                        # Game over - red flash and restart
+                        rect(0, 0, WIDTH - 1, (HEIGHT-6) - 1, *inactive_colors[0])
+                        display_score_and_time(0)
+                        start_game()
+
                 else:
+                    print("Invalid input")
                     break
-
-            if not check_user_sequence():
-                start_game()
-
+                
             time.sleep(1)
 
     main_simon_game_loop()
 
 # Snake Game
 def snake_game():
-    rtc = machine.RTC()
-
     def hsb_to_rgb(hue, saturation, brightness):
         hue_normalized = (hue % 360) / 60
         hue_index = int(hue_normalized)
@@ -499,9 +560,9 @@ def snake_game():
             time.sleep(max(0.03, (0.09 - max(0.01, snake_length / 300))))
 
     restart_game()
-    place_target()
     main_snake_game_loop()
 
 # Main
 if __name__ == '__main__':
     game_selector()
+    
